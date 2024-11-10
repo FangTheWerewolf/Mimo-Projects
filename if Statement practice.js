e015c7957fb181fb65e637cfe9f2ea5a4fc18f17
FangTheWerewolf <cfls3264@gmail.com>
2024-11-10 17:17:07 -0500

    Update if Statement practice.js

--- if Statement practice.js
+++ if Statement practice.js
@@ -33,3 +33,23 @@ if (hour < 12) {
 } else if (hour = 17) [
     console.log("Good afternoon!")
 ]
+
+let hour = 18;
+if (hour < 12) {
+    console.log("good morning!");
+} else if (hour < 17) {
+    console.log("Good afternoon!");
+} else {
+    console.log("Good night!")
+}
+
+let hour = 20;
+if (hour < 12) {
+    console.log("Good morning!");
+} else if (hour < 17) {
+    console.log("Good afternoon!");
+} else if (hour < 21) {
+    console.log("Good evening!");
+} else {
+    console.log("Good night!");
+}
