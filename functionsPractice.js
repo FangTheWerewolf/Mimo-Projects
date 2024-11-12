f3ecfa48c4c79fb052ecb55e51d254a36c50b997
FangTheWerewolf <cfls3264@gmail.com>
2024-11-12 16:30:11 -0500

    Create functionsPractice.js

---
+++ functionsPractice.js
@@ -0,0 +1,10 @@
+function greetUser() {
+    console.log("Good morning, Anna!");
+    console.log("Welcome back!");
+}
+
+function weatherUpdate() {
+    console.log("Weather update");
+    console.log("Sunny skies ahead");
+}
+weatherUpdate();
